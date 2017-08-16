6b683f33144c7a0c0e90caa12ddcf31377883dcc
francisud <francis.deoferio@gmail.com>
2017-08-17 01:23:53 +0800

    New Approach

---
+++ SPNEW/app/src/main/java/com/example/fud/spnew/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.fud.spnew;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.util.Log;
+
+import org.opencv.android.OpenCVLoader;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    // Used to load the 'native-lib' library on application startup.
+    static {
+        System.loadLibrary("native-lib");
+
+        if(OpenCVLoader.initDebug()){
+            Log.d(TAG, "OPENCV WOOOOOOOOORKING!@#!@#!@");
+        }else{
+            Log.d(TAG, "opencv not working");
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Example of a call to a native method
+        TextView tv = (TextView) findViewById(R.id.sample_text);
+        tv.setText(stringFromJNI());
+    }
+
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+    public native String stringFromJNI();
+}
