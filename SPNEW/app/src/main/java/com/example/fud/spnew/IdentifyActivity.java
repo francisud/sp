c9131c3c6762b518b899d8025488a286ed65876b
francisud <francis.deoferio@gmail.com>
2017-08-18 01:16:52 +0800

    Added alert dialog for picture source
    Todo - open camera and open file browser

--- SPNEW/app/src/main/java/com/example/fud/spnew/IdentifyActivity.java
+++ SPNEW/app/src/main/java/com/example/fud/spnew/IdentifyActivity.java
@@ -1,11 +1,15 @@
 package com.example.fud.spnew;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class IdentifyActivity extends AppCompatActivity {
 
@@ -25,6 +29,21 @@ public class IdentifyActivity extends AppCompatActivity {
             }
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
     }
+
+    public void createDialog(View view) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(IdentifyActivity.this);
+        builder.setTitle(R.string.pick_action);
+        builder.setItems(R.array.actions_array, new DialogInterface.OnClickListener(){
+            public void onClick(DialogInterface dialog, int which){
+            }
+        });
+
+        AlertDialog dialog = builder.create();
+
+        dialog.show();
+    }
+
 
 }
