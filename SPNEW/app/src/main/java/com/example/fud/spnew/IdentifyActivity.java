f3b82390cfcda2fb4194f396f66f4860982de750
francisud <francis.deoferio@gmail.com>
2017-08-24 02:16:52 +0800

    Working camera source with save, need to check

--- SPNEW/app/src/main/java/com/example/fud/spnew/IdentifyActivity.java
+++ SPNEW/app/src/main/java/com/example/fud/spnew/IdentifyActivity.java
@@ -3,10 +3,13 @@ package com.example.fud.spnew;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -16,6 +19,13 @@ import android.net.Uri;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.graphics.Bitmap;
+import android.content.Context;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class IdentifyActivity extends AppCompatActivity {
 
@@ -70,7 +80,21 @@ public class IdentifyActivity extends AppCompatActivity {
                 if(which == 0){
                     intent.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
                     if (intent.resolveActivity(getPackageManager()) != null) {
-                        startActivityForResult(intent, 0);
+                        // Create the File where the photo should go
+                        File photoFile = null;
+                        try {
+                            photoFile = createImageFile();
+                        } catch (IOException ex) {
+                            // Error occurred while creating the File
+                        }
+                        // Continue only if the File was successfully created
+                        if (photoFile != null) {
+                            Uri photoURI = FileProvider.getUriForFile(getApplication().getApplicationContext(),
+                                    "com.example.fud.spnew.fileprovider",
+                                    photoFile);
+                            intent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                            startActivityForResult(intent, 0);
+                        }
                     }
                 }
 
@@ -87,18 +111,72 @@ public class IdentifyActivity extends AppCompatActivity {
         dialog.show();
     }
 
+
+    String mCurrentPhotoPath;
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+
+
+
+
+
+
+
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
+
 
         //camera
         if (requestCode == 0 && resultCode == RESULT_OK) {
-            Bundle extras = data.getExtras();
-            Bitmap imageBitmap = (Bitmap) extras.get("data");
-            if(source.equals("top"))
-                top.setImageBitmap(imageBitmap);
-            if(source.equals("side"))
-                side.setImageBitmap(imageBitmap);
-            if(source.equals("bottom"))
-                bottom.setImageBitmap(imageBitmap);
+//            Bitmap imageBitmap = null;
+//            try{
+//                imageBitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), Uri.parse(mCurrentPhotoPath));
+//            }catch(IOException e){
+//                Toast.makeText(this, e.toString(), Toast.LENGTH_LONG).show();
+//            }
+
+            // Get the dimensions of the View
+            int targetW = top.getWidth();
+            int targetH = top.getHeight();
+
+            // Get the dimensions of the bitmap
+            BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+            bmOptions.inJustDecodeBounds = true;
+            BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
+            int photoW = bmOptions.outWidth;
+            int photoH = bmOptions.outHeight;
+
+            // Determine how much to scale down the image
+            int scaleFactor = Math.min(photoW/targetW, photoH/targetH);
+
+            // Decode the image file into a Bitmap sized to fill the View
+            bmOptions.inJustDecodeBounds = false;
+            bmOptions.inSampleSize = scaleFactor;
+            bmOptions.inPurgeable = true;
+
+            Bitmap bitmap = BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
+            top.setImageBitmap(bitmap);
+
+
+//            if(source.equals("top"))
+//                top.setImageBitmap(imageBitmap);
+//            if(source.equals("side"))
+//                side.setImageBitmap(imageBitmap);
+//            if(source.equals("bottom"))
+//                bottom.setImageBitmap(imageBitmap);
         }
 
 
@@ -106,10 +184,14 @@ public class IdentifyActivity extends AppCompatActivity {
         if (requestCode == 1 && resultCode == RESULT_OK) {
             Uri selectedImageUri = data.getData();
             selectedImagePath = selectedImageUri.getPath();
-            //img.setImageURI(selectedImageUri);
+
+            if(source.equals("top"))
+                top.setImageURI(selectedImageUri);
+            if(source.equals("side"))
+                side.setImageURI(selectedImageUri);
+            if(source.equals("bottom"))
+                bottom.setImageURI(selectedImageUri);
         }
-
-
 
     }
 
