10e456e928adb1241ae6e6be67819ac72bd38bd7
francisud <francis.deoferio@gmail.com>
2017-09-25 16:34:06 +0800

    Faster loading of image, drawable rectangle

--- SPNEW/app/src/main/java/com/example/fud/spnew/CropActivity.java
+++ SPNEW/app/src/main/java/com/example/fud/spnew/CropActivity.java
@@ -4,90 +4,59 @@ import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.media.Image;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Point;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
-import android.view.MenuItem;
+import android.view.Display;
 import android.view.View;
-import android.widget.ImageButton;
 import android.widget.ImageView;
-import android.widget.TextView;
-import android.widget.Toast;
 
 public class CropActivity extends AppCompatActivity {
-
-    private TextView mTextMessage;
-
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
-                    return true;
-            }
-            return false;
-        }
-
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crop);
 
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-
+        //get photopath
         Bundle extras = getIntent().getExtras();
-
         String photoPath = extras.getString("photoPath");
 
-//        Toast.makeText(CropActivity.this, photoPath,
-//                Toast.LENGTH_LONG).show();
-//
+        //display image, and resize to have faster loading
+        //guide - http://www.informit.com/articles/article.aspx?p=2143148&seqNum=2
+        Point size = new Point();
+
         ImageView iv = (ImageView) findViewById(R.id.imageView3);
-        iv.setImageBitmap(BitmapFactory.decodeFile(photoPath));
-//
-//        int targetW = -1;
-//        int targetH = -1;
-//
-//        // Get the dimensions of the View
-//        targetW = iv.getWidth();
-//        targetH = iv.getHeight();
-//
-//        // Get the dimensions of the bitmap
-//        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
-//        bmOptions.inJustDecodeBounds = true;
-//
-//        BitmapFactory.decodeFile(photoPath, bmOptions);
-//        int photoW = bmOptions.outWidth;
-//        int photoH = bmOptions.outHeight;
-//
-//        // Determine how much to scale down the image
-//        int scaleFactor = Math.min(photoW/targetW, photoH/targetH);
-//
-//        // Decode the image file into a Bitmap sized to fill the View
-//        bmOptions.inJustDecodeBounds = false;
-////        bmOptions.inSampleSize = scaleFactor;
-////
-////        Bitmap bitmap = BitmapFactory.decodeFile(photoPath, bmOptions);
-////
-////        iv.setImageBitmap(bitmap);
+        Display display = getWindowManager().getDefaultDisplay();
+        display.getSize(size);
+        int displayWidth = size.x;
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        options.inMutable = true;
+        BitmapFactory.decodeFile(photoPath, options);
+        int width = options.outWidth;
+          if (width > displayWidth) {
+              int widthRatio = Math.round((float) width / (float) displayWidth);
+              options.inSampleSize = widthRatio;
+          }
+        options.inJustDecodeBounds = false;
+        Bitmap scaledBitmap =  BitmapFactory.decodeFile(photoPath, options);
+//        iv.setImageBitmap(scaledBitmap);
+
+        Paint paint=new Paint();
+        paint.setColor(Color.RED);
+
+        Canvas canvas = new Canvas(scaledBitmap);
+        canvas.drawBitmap(scaledBitmap, 0, 0, null);
+        canvas.drawRect(20,20,50,50, paint);
+        iv.setImageBitmap(scaledBitmap);
     }
 
 
-
+    //return the coordinates of the rectangle
     public void toReturn(View view){
         Intent returnIntent = new Intent();
         returnIntent.putExtra("result", "result");
